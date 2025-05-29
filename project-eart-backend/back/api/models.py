1539d3585ee18c84817dd942c706f003d6889de4
haroldpp22 <hpotosme@fwdcostarica.com>
2025-05-29 13:50:34 -0600

    backend290525150

---
+++ project-eart-backend/back/api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Publicaciones(models.Model):
+  fechaPublicaion = models.DateField()
+  publicacionFoto = models.TextField()
+  publicacion = models.TextField()
+
+class Reacciones(models.Model):
+  numeroDeReacciones = models.IntegerField()
+
+class Comentarios(models.Model):
+  fechaComentarios = models.DateField()
+  comentarios = models.CharField(max_length=100,blank=True)
+
+class CustomUser(AbstractUser):
+  Reacciones = models.ForeignKey(Reacciones, on_delete=models.CASCADE)
+  publicaciones = models.ForeignKey(Publicaciones, on_delete=models.CASCADE)
+  Comentarios = models.ForeignKey(Comentarios, on_delete=models.CASCADE)
+  fechaNacimiento = models.DateField()
+  
+class Mensajes(models.Model):
+  fechaMensajes = models.DateField()
+  mensajes = models.CharField(max_length=100,blank=True)
+
+  
+
+
+
+
+
+
+
+
+
+# Create your models here.
