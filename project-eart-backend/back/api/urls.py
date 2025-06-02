af3bcb01ff32bfec55fdaebc5cee7a026379dc60
haroldpp22 <hpotosme@fwdcostarica.com>
2025-06-02 11:18:03 -0600

    backend02062511:17

--- project-eart-backend/back/api/urls.py
+++ project-eart-backend/back/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PublicacionesListCreateVew
+from .views import PublicacionesListCreateVew,PublicacionesDetailView
 
 
 urlpatterns = [
  
-        path('Publicaciones/',PublicacionesListCreateVew.as_view(), name='Publicaciones-listar-crear')
-
+        path('Publicaciones/',PublicacionesListCreateVew.as_view(), name='Publicaciones-listar-crear'),
+        path('Publicaciones/<int:pk>',PublicacionesDetailView.as_view(), name='Publicacion-listar-actualizar')
 ]
